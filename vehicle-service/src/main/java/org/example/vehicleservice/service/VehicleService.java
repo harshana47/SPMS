c2ba31a74da163c929d7b4cc18ef7dd3de45e4f6
harshana47 <pabodhaharshana15.com>
2025-06-07 21:45:00 +0530

    working

---
+++ vehicle-service/src/main/java/org/example/vehicleservice/service/VehicleService.java
@@ -0,0 +1,18 @@
+package org.example.vehicleservice.service;
+
+import org.example.vehicleservice.entity.Vehicle;
+
+import java.util.List;
+
+public interface VehicleService {
+
+    Vehicle registerVehicle(Vehicle vehicle);
+
+    List<Vehicle> getVehiclesByUserId(Long userId);
+
+    Vehicle updateVehicle(Long id, Vehicle vehicle);
+
+    Vehicle simulateEntry(Long id);
+
+    Vehicle simulateExit(Long id);
+}
